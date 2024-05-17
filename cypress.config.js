43e5db7bcef5d1c6acd0e9264f6f2b0d83b937e9
Rossa Afresia Siagian <rossaafresiasiagian@Rossa-Afresia-Siagian.local>
2024-05-18 01:23:44 +0700

    Add file configuration -  base url

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+
+module.exports = {
+  e2e: {
+    baseUrl: 'https://magento.softwaretestingboard.com',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+  },
+}
+
+
